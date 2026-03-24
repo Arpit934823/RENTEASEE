a806e542654e211b8000771ea70ed20c28ebc380
Your Name <you@example.com>
2026-03-24 22:42:13 +0530

    chore: prepare project for web deployment (Firebase + Netlify)

---
+++ lib/firebase_options.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+      case TargetPlatform.windows:
+      case TargetPlatform.linux:
+      case TargetPlatform.fuchsia:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyC-x2DSdBYhlAzVoHIg2llV0RRybHPeVvk',
+    appId: '1:101340260529:android:6b12485e2daff841bf9a90',
+    messagingSenderId: '101340260529',
+    projectId: 'rentease-1bf37',
+    storageBucket: 'rentease-1bf37.firebasestorage.app',
+  );
+}
